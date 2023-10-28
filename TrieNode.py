f02313ce98c5af1a75996f8681c89a8f89961298
hai nguyen <haiknt2002@gmail.com>
2023-10-28 10:27:56 +0700

    2nd commit

--- TrieNode.py
+++ TrieNode.py
@@ -1,40 +1,44 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}  # Một từ điển (dictionary) để lưu trữ các con của nút hiện tại.
+        self.children = {}  # Một từ điển (dictionary) để lưu trữ chỉ số của các nút con.
         self.is_end_of_word = False  # Đánh dấu xem nút hiện tại có phải là cuối của một từ hay không.
 
 class Trie:
     def __init__(self):
-        self.root = TrieNode()  # Khởi tạo cây tiền tố với một nút gốc (root).
+        self.nodes = [TrieNode()]  # Sử dụng một danh sách để lưu trữ các nút.
 
     def insert(self, word):
-        node = self.root
+        node_index = 0  # Bắt đầu từ nút gốc.
         for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()  # Nếu ký tự char chưa tồn tại trong các con của nút hiện tại, thêm một nút con mới.
-            node = node.children[char]  # Di chuyển đến nút con tương ứng với ký tự char.
-        node.is_end_of_word = True  # Đánh dấu nút hiện tại là cuối của một từ.
+            if char not in self.nodes[node_index].children:
+                new_node_index = len(self.nodes)
+                self.nodes[node_index].children[char] = new_node_index  # Thêm chỉ số của nút con mới.
+                self.nodes.append(TrieNode())  # Thêm một nút con mới vào danh sách.
+            node_index = self.nodes[node_index].children[char]  # Di chuyển đến nút con tương ứng.
+        self.nodes[node_index].is_end_of_word = True  # Đánh dấu nút hiện tại là cuối của một từ.
 
     def search(self, word):
-        node = self.root
+        node_index = 0  # Bắt đầu từ nút gốc.
         for char in word:
-            if char not in node.children:
+            if char not in self.nodes[node_index].children:
                 return False  # Nếu có ký tự không tồn tại trong cây, từ không tồn tại.
-            node = node.children[char]
-        return node.is_end_of_word  # Trả về True nếu nút hiện tại là cuối của một từ, tức là từ tồn tại.
+            node_index = self.nodes[node_index].children[char]
+        return self.nodes[node_index].is_end_of_word  # Trả về True nếu nút hiện tại là cuối của một từ, tức là từ tồn tại.
 
 def main():
+    trie = Trie()
     words_to_insert = ["the", "a", "an", "there", "answer", "any", "by", "bye", "their"]
-    trie = Trie()  # Tạo một cây tiền tố mới.
 
     for word in words_to_insert:
         trie.insert(word)  # Chèn từng từ từ danh sách vào cây tiền tố.
 
     words_to_search = ["the", "answer", "hello", "by", "thermometer"]
-    
+
     for word in words_to_search:
         if trie.search(word):
             print(f"'{word}' found in the trie.")  # Kiểm tra xem từ có tồn tại trong cây tiền tố không và in kết quả.
+        else:
+            print(f"'{word}' not found in the trie.")
 
 if __name__ == "__main__":
     main()
